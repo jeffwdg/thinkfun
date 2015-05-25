4190fb016678155fe2e7cba3fe13c5bc049e782f
jeffwdg <aloofzehcnas@gmail.com>
2015-05-25 13:16:14 +0800

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -266,7 +266,7 @@ function updateLeaderboardTime(ename, uname, solvetime, totalsolve){
 	 var data = { email: ename, uname: uname, solvetime: totalsolve };
 
 	$.ajax({
-		url: "https://thinkfun.mybluemix.net/updatepoints/"+uname+"/"+ename+"/"+solvetime+"/"+totalsolve,
+		url: "https://thinkfun.mybluemix.net/updateboard/"+uname+"/"+ename+"/"+solvetime+"/"+totalsolve,
 		type: 'post',
 		dataType: 'html',
 		success: function(results){
